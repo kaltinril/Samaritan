83621a9ac5fb6ed5ec102b8461c9e8f40ebff0b4
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2017-12-03 21:14:57 -0900

    New blank activity for creating a profile for the first time

---
+++ app/src/main/java/com/kaltinril/android/samaritan/CreateProfile.java
@@ -0,0 +1,13 @@
+package com.kaltinril.android.samaritan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_profile);
+    }
+}
